f8524f22121252e812a064a28080ae4f62152f9a
GranainoBUAB <josemiguel@quesada.eu>
2024-10-08 14:34:43 +0200

    add innerHTML

--- js/index.js
+++ js/index.js
@@ -9,9 +9,23 @@ async function fetchHousesJson(){
 
 fetchHousesJson().then(house => {
     for (let index=0; index < house.houses.length ; index++) {
-        let price  = house.houses[index].price;
-        console.log(price);
 
+        let id  = house.houses[index].id;
+        let image  = house.houses[index].image;
+        let price  = house.houses[index].price;
+        let city  = house.houses[index].city;
+        let description  = house.houses[index].description;
+        
+        houseSection.innerHTML += `
+            
+            <img src="${image}" alt="">
+            <div>id. ${id}</div>
+            <p>El Precio es ${price}</p>
+            <p>Esta en ${city}</p>
+            <div>info : ${description}</div>
+
+        `
 
     }
 })
+
